2440725134ae889fd251d8d88e951ec49e09946f
FerAC <87688596+FerAC@users.noreply.github.com>
2025-05-04 16:14:26 -0600

    feat(profile): update ProfileScreen layout and add action buttons for creating and modifying posts

--- lib/src/profile/presenter/page/page.dart
+++ lib/src/profile/presenter/page/page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/core/theme/app_colors.dart';
+import 'package:mobile/core/globals/widgets/secondary_button.dart';
+
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -7,17 +8,106 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: const Center(
-        child: Text(
-          'Profile screen',
-          style: TextStyle(
-            fontSize: 20,
-            color: AppColors.textPrimary,
-            fontWeight: FontWeight.w500,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Fernando Arce',
+                          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          '@ferac',
+                          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'fernando.arcecastillo@ucr.ac.cr',
+                          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: Theme.of(context).colorScheme.outline,
+                          ),
+                        ),
+                        const SizedBox(height: 18),
+                        
+                        Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Flexible(child: _buildCreatePostButton()),
+                          const SizedBox(width: 8),
+                          Flexible(child: _buildModifyButton()),
+                        ],
+                      ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(width: 16),
+
+                  CircleAvatar(
+                    radius: 35,
+                    // backgroundImage: AssetImage('assets/profile.jpg'),
+                    backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              Divider(color: Theme.of(context).colorScheme.outline),
+
+              // TODO: Implement posts section
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'No posts yet',
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildModifyButton() {
+    return SecondaryButton(
+      onPressed: () {
+        // TODO: Implement modify profile functionality
+      },
+      isLoading: false,
+      text: 'Edit Profile',
+      isEnabled: true,
+      height: 32,
+      width: 160,
+    );
+  }
+
+  Widget _buildCreatePostButton() {
+    return SecondaryButton(
+      onPressed: () {
+        // TODO: Implement create post functionality
+      },
+      isLoading: false,
+      text: 'New Post',
+      isEnabled: true,
+      height: 32,
+      width: 160,
     );
   }
 }
