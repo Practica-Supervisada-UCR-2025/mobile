f1cc198eecb3f521d736ba8de87599b4a8a389e0
FerAC <87688596+FerAC@users.noreply.github.com>
2025-05-04 21:54:32 -0600

    fix(main): correct typo in build method parameter

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContextcontext) {
+  Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
         RepositoryProvider<RegisterRepository>(
