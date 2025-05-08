04bbf88b90d296f551769d3075f44c4176569d71
rlop_03 <randall.lopezvarela@ucr.ac.cr>
2025-05-08 15:49:52 -0600

    fix(navigation): fix navigation in forgot_password using router instead of navigation

--- test/src/home/presenter/page/page_test.dart
+++ test/src/home/presenter/page/page_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile/core/theme/app_colors.dart';
-import 'package:mobile/src/home/presenter/page/page.dart';
+import 'package:mobile/src/home/presenter/page/page.dart'; 
 
 void main() {
-  testWidgets('HomeScreen displays "Home screen" text', (tester) async {
+  testWidgets('HomeScreen displays "Home" text', (tester) async {
     await tester.pumpWidget(
       MaterialApp(
         home: HomeScreen(),
