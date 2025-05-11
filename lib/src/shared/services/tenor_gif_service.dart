e54e26de34145bc56b4900edb816a8555c91a565
brillygamboa <BRILLY.GAMBOA@ucr.ac.cr>
2025-05-11 10:17:05 -0600

    refactor(tenor_gif_service): clean up code formatting and improve readability

--- lib/src/shared/services/tenor_gif_service.dart
+++ lib/src/shared/services/tenor_gif_service.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-import '../models/gif_model.dart'; // lo crearemos abajo
+import '../models/gif_model.dart'; 
 
 class TenorGifService {
   final String _baseUrl = 'https://tenor.googleapis.com/v2';
-  final String _apiKey = 'AIzaSyArCGZttLRT0dmRVv-EBdUHcTzErxojHB0'; // tu API key
+  final String _apiKey = 'AIzaSyArCGZttLRT0dmRVv-EBdUHcTzErxojHB0'; 
 
   Future<List<GifModel>> searchGifs(String query, {int limit = 20}) async {
     final response = await http.get(Uri.parse(
       '$_baseUrl/search?q=$query&key=$_apiKey&limit=$limit&media_filter=tinygif',
     ));
+
+    print('RESPONSE: ${response.body}'); 
 
     if (response.statusCode == 200) {
       final List results = json.decode(response.body)['results'];
@@ -21,9 +23,12 @@ class TenorGifService {
   }
 
   Future<List<GifModel>> getTrendingGifs({int limit = 20}) async {
-    final response = await http.get(Uri.parse(
-      '$_baseUrl/trending?key=$_apiKey&limit=$limit&media_filter=tinygif',
-    ));
+    final response = await http.get(
+      Uri.parse('$_baseUrl/trending?key=$_apiKey&limit=$limit&media_filter=tinygif',),
+       headers: {
+          'Accept': 'application/json',
+        },
+      );
 
     if (response.statusCode == 200) {
       final List results = json.decode(response.body)['results'];
