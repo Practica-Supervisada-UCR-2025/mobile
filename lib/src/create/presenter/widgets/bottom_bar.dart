eafb61c37307046cf95d874d98a1a49673e18754
gqbo <gabriel.gonzaf@gmail.com>
2025-05-11 23:40:48 -0600

    feat(create_post): add image selection functionality from gallery and camera

--- lib/src/create/presenter/widgets/bottom_bar.dart
+++ lib/src/create/presenter/widgets/bottom_bar.dart
@@ -1,10 +1,54 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mobile/core/core.dart';
 import 'package:mobile/src/create/create.dart';
 
 class BottomBar extends StatelessWidget {
-  const BottomBar({super.key});
+  final Function(File?) onImageSelected;
 
+  const BottomBar({super.key, required this.onImageSelected});
+
+  Future<void> _pickImageFromGallery(BuildContext context) async {
+    final permissionsRepo = context.read<PermissionsRepository>();
+    bool hasPermission = await permissionsRepo.checkGalleryPermission(
+      context: context,
+    );
+
+    if (hasPermission) {
+      final ImagePicker picker = ImagePicker();
+      final XFile? image = await picker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 80,
+      );
+
+      if (image != null) {
+        onImageSelected(File(image.path));
+      }
+    }
+  }
+
+  Future<void> _takePhoto(BuildContext context) async {
+    final permissionsRepo = context.read<PermissionsRepository>();
+    bool hasPermission = await permissionsRepo.checkCameraPermission(
+      context: context,
+    );
+
+    if (hasPermission) {
+      final ImagePicker picker = ImagePicker();
+      final XFile? photo = await picker.pickImage(
+        source: ImageSource.camera,
+        imageQuality: 80,
+      );
+
+      if (photo != null) {
+        onImageSelected(File(photo.path));
+      }
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,9 +64,11 @@ class BottomBar extends StatelessWidget {
         children: [
           IconButton(
             icon: const Icon(Icons.image_outlined),
-            onPressed: () {
-              // Action to add an image
-            },
+            onPressed: () => _pickImageFromGallery(context),
+          ),
+          IconButton(
+            icon: const Icon(Icons.camera_alt_outlined),
+            onPressed: () => _takePhoto(context),
           ),
           const Spacer(),
           BlocBuilder<CreatePostBloc, CreatePostState>(
