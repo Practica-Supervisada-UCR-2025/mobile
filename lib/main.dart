f6e516291c1c8e8d87188ca80407a25bb7b60749
brillygamboa <BRILLY.GAMBOA@ucr.ac.cr>
2025-05-12 23:38:11 -0600

    fix(main.dart): ensure dotenv is loaded in main function

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:mobile/src/auth/_children/_children.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/core.dart';
 import 'firebase_options.dart';
 
@@ -13,7 +14,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await LocalStorage.init();
-
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
