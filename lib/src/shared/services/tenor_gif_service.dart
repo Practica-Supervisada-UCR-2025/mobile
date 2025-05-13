88408305e1c72f0e8b33fdf29f79c0fa90357625
brillygamboa <BRILLY.GAMBOA@ucr.ac.cr>
2025-05-12 23:38:41 -0600

    fix(tenor_gif_service): replace hardcoded API key with dotenv variable and improve error logging

--- lib/src/shared/services/tenor_gif_service.dart
+++ lib/src/shared/services/tenor_gif_service.dart
@@ -1,39 +1,48 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
-import '../models/gif_model.dart'; 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../models/gif_model.dart';
 
 class TenorGifService {
   final String _baseUrl = 'https://tenor.googleapis.com/v2';
-  final String _apiKey = 'AIzaSyArCGZttLRT0dmRVv-EBdUHcTzErxojHB0'; 
+
+final String _apiKey = dotenv.env['TENOR_API_KEY'] ?? '';
+
+  final String _clientKey = 'ucr_conect';
 
   Future<List<GifModel>> searchGifs(String query, {int limit = 20}) async {
     final response = await http.get(Uri.parse(
-      '$_baseUrl/search?q=$query&key=$_apiKey&limit=$limit&media_filter=tinygif',
+      '$_baseUrl/search?q=$query&key=$_apiKey&client_key=$_clientKey&limit=$limit&media_filter=tinygif',
     ));
 
-    print('RESPONSE: ${response.body}'); 
+    print('RESPONSE: ${response.body}');
 
     if (response.statusCode == 200) {
       final List results = json.decode(response.body)['results'];
       return results.map((e) => GifModel.fromJson(e)).toList();
     } else {
+      print('Failed to load GIFs. Status code: ${response.statusCode}');
+      print('Body: ${response.body}');
       throw Exception('Failed to load GIFs');
     }
   }
 
   Future<List<GifModel>> getTrendingGifs({int limit = 20}) async {
     final response = await http.get(
-      Uri.parse('$_baseUrl/featured?key=$_apiKey&limit=$limit&media_filter=tinygif'),
-       headers: {
-          'Accept': 'application/json',
-        },
-      );
+      Uri.parse(
+        '$_baseUrl/featured?key=$_apiKey&client_key=$_clientKey&limit=$limit&media_filter=tinygif',
+      ),
+      headers: {
+        'Accept': 'application/json',
+      },
+    );
 
     if (response.statusCode == 200) {
       final List results = json.decode(response.body)['results'];
       return results.map((e) => GifModel.fromJson(e)).toList();
     } else {
+      print('Failed to load trending GIFs. Status code: ${response.statusCode}');
+      print('Body: ${response.body}');
       throw Exception('Failed to load trending GIFs');
     }
   }
