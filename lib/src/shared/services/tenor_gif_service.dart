2fae44065bd5587cfb0a6cffd7e721158c1da56d
brillygamboa <BRILLY.GAMBOA@ucr.ac.cr>
2025-05-14 18:06:41 -0600

    fix(tenor_gif_service): update searchGifs and getTrendingGifs methods to support pagination

--- lib/src/shared/services/tenor_gif_service.dart
+++ lib/src/shared/services/tenor_gif_service.dart
@@ -1,18 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../models/trending_response.dart';
 import '../models/gif_model.dart';
 
 class TenorGifService {
   final String _baseUrl = 'https://tenor.googleapis.com/v2';
-
-final String _apiKey = dotenv.env['TENOR_API_KEY'] ?? '';
-
+  final String _apiKey = dotenv.env['TENOR_API_KEY'] ?? '';
   final String _clientKey = 'ucr_conect';
 
-  Future<List<GifModel>> searchGifs(String query, {int limit = 20}) async {
+  Future<List<GifModel>> searchGifs(String query, {int limit = 20, int pos = 0}) async {
     final response = await http.get(Uri.parse(
-      '$_baseUrl/search?q=$query&key=$_apiKey&client_key=$_clientKey&limit=$limit&media_filter=tinygif',
+      '$_baseUrl/search?q=$query&key=$_apiKey&client_key=$_clientKey'
+      '&limit=$limit&pos=$pos&media_filter=tinygif',
     ));
 
     print('RESPONSE: ${response.body}');
@@ -27,19 +27,22 @@ final String _apiKey = dotenv.env['TENOR_API_KEY'] ?? '';
     }
   }
 
-  Future<List<GifModel>> getTrendingGifs({int limit = 20}) async {
+  Future<TrendingGifResponse> getTrendingGifs({int limit = 20, String? pos}) async {
     final response = await http.get(
       Uri.parse(
-        '$_baseUrl/featured?key=$_apiKey&client_key=$_clientKey&limit=$limit&media_filter=tinygif',
+        '$_baseUrl/featured?key=$_apiKey&client_key=$_clientKey'
+        '&limit=$limit${pos != null ? '&pos=$pos' : ''}&media_filter=tinygif',
       ),
-      headers: {
-        'Accept': 'application/json',
-      },
+      headers: {'Accept': 'application/json'},
     );
 
     if (response.statusCode == 200) {
-      final List results = json.decode(response.body)['results'];
-      return results.map((e) => GifModel.fromJson(e)).toList();
+      final decoded = json.decode(response.body);
+      final List results = decoded['results'];
+      final next = decoded['next'];
+
+      final gifs = results.map((e) => GifModel.fromJson(e)).toList();
+      return TrendingGifResponse(gifs: gifs, next: next);
     } else {
       print('Failed to load trending GIFs. Status code: ${response.statusCode}');
       print('Body: ${response.body}');
