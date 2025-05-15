b2f87df47152c4fe1a693851ac9f13e8b3b01b3a
brillygamboa <BRILLY.GAMBOA@ucr.ac.cr>
2025-05-14 20:59:32 -0600

    fix(create_post_state): include selectedGif in state management and copyWith methods

--- lib/src/create/presenter/bloc/create_post_state.dart
+++ lib/src/create/presenter/bloc/create_post_state.dart
@@ -4,20 +4,49 @@ sealed class CreatePostState extends Equatable {
   final String text;
   final bool isOverLimit;
   final bool isValid;
+  final GifModel? selectedGif;
 
   const CreatePostState({
     required this.text,
     required this.isOverLimit,
     required this.isValid,
+    required this.selectedGif,
   });
 
   @override
-  List<Object> get props => [text, isOverLimit, isValid];
+  List<Object?> get props => [text, isOverLimit, isValid, selectedGif];
+
+  CreatePostState copyWith({
+    String? text,
+    bool? isOverLimit,
+    bool? isValid,
+    GifModel? selectedGif,
+  });
 }
 
 final class CreatePostInitial extends CreatePostState {
   const CreatePostInitial()
-      : super(text: '', isOverLimit: false, isValid: false);
+      : super(
+          text: '',
+          isOverLimit: false,
+          isValid: false,
+          selectedGif: null,
+        );
+
+  @override
+  CreatePostChanged copyWith({
+    String? text,
+    bool? isOverLimit,
+    bool? isValid,
+    GifModel? selectedGif,
+  }) {
+    return CreatePostChanged(
+      text: text ?? this.text,
+      isOverLimit: isOverLimit ?? this.isOverLimit,
+      isValid: isValid ?? this.isValid,
+      selectedGif: selectedGif ?? this.selectedGif,
+    );
+  }
 }
 
 final class CreatePostChanged extends CreatePostState {
@@ -25,5 +54,21 @@ final class CreatePostChanged extends CreatePostState {
     required super.text,
     required super.isOverLimit,
     required super.isValid,
+    required super.selectedGif,
   });
+
+  @override
+  CreatePostChanged copyWith({
+    String? text,
+    bool? isOverLimit,
+    bool? isValid,
+    GifModel? selectedGif,
+  }) {
+    return CreatePostChanged(
+      text: text ?? this.text,
+      isOverLimit: isOverLimit ?? this.isOverLimit,
+      isValid: isValid ?? this.isValid,
+      selectedGif: selectedGif ?? this.selectedGif,
+    );
+  }
 }
