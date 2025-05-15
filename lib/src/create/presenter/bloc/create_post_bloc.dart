bd33ac8493bf8604cf6f5b9b043e3f80fc085d42
brillygamboa <BRILLY.GAMBOA@ucr.ac.cr>
2025-05-14 20:59:17 -0600

    fix(create_post_bloc): add GIF selection handling in CreatePostBloc

--- lib/src/create/presenter/bloc/create_post_bloc.dart
+++ lib/src/create/presenter/bloc/create_post_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:mobile/src/shared/models/gif_model.dart';
 
 part 'create_post_event.dart';
 part 'create_post_state.dart';
@@ -9,6 +10,9 @@ class CreatePostBloc extends Bloc<CreatePostEvent, CreatePostState> {
 
   CreatePostBloc() : super(const CreatePostInitial()) {
     on<PostTextChanged>(_onTextChanged);
+    on<GifSelected>((event, emit) {
+      emit(state.copyWith(selectedGif: event.gif));
+    });
   }
 
   void _onTextChanged(PostTextChanged event, Emitter<CreatePostState> emit) {
@@ -20,6 +24,7 @@ class CreatePostBloc extends Bloc<CreatePostEvent, CreatePostState> {
       text: text,
       isOverLimit: isOverLimit,
       isValid: isValid,
+      selectedGif: state.selectedGif,
     ));
   }
 }
