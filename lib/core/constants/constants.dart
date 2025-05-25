34e79146e2282506c861c0d490b0e53ffbe43f90
gqbo <gabriel.gonzaf@gmail.com>
2025-05-24 19:45:38 -0600

    feat(create_post): add API_POST_BASE_URL constant for post API

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,6 +2,8 @@
 
 const API_BASE_URL = 'http://157.230.224.13:3000/api/';
 
+const API_POST_BASE_URL = 'http://157.230.224.13:3003/api/';
+
 const IMAGES_ALLOWED = ['png', 'jpg', 'jpeg', 'webp'];
 
 const MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5 MB
