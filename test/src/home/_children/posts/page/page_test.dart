5bf0dca2383af9afd95c9987538f408ed087995d
Pochix14 <prodriguez99@hotmail.com>
2025-06-03 18:16:35 -0600

    test(homepage): delete useless comments

--- test/src/home/_children/posts/page/page_test.dart
+++ test/src/home/_children/posts/page/page_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:mobile/core/globals/publications/publications.dart';
 import 'package:mobile/src/home/home.dart';
 
