143fa881234d2f705f0637f32a665c35e095fffc
gqbo <gabriel.gonzaf@gmail.com>
2025-06-11 16:57:53 -0600

    refactor(comments): change CommentsRepository to an abstract class with respective methods

--- lib/src/comments/domain/repository/comments_repository.dart
+++ lib/src/comments/domain/repository/comments_repository.dart
@@ -1,27 +1,8 @@
-import 'package:http/http.dart' as http;
-import 'package:mobile/src/comments/domain/models/comments_response.dart';
-import 'dart:convert';
+import '../models/comments_response.dart';
 
-class CommentsRepository {
-  final http.Client client;
-
-  CommentsRepository({http.Client? client}) : client = client ?? http.Client();
-
+abstract class CommentsRepository {
   Future<CommentsResponse> fetchComments({
     required String postId,
     required DateTime startTime,
-  }) async {
-    final uri = Uri.parse(
-      'http://192.168.100.17:3000/api/posts/$postId/comments?startTime=${startTime.toUtc().toIso8601String()}',
-    );
-
-    final response = await client.get(uri);
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return CommentsResponse.fromJson(data);
-    } else {
-      throw Exception("Error loading comments");
-    }
-  }
+  });
 }
