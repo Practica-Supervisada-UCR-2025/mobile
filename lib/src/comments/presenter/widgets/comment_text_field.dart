5287f550cd0bc299f7686f897e558468f2b1b7f4
gqbo <gabriel.gonzaf@gmail.com>
2025-06-11 18:02:47 -0600

    feat(comments): add CommentTextField widget for user input in comments

---
+++ lib/src/comments/presenter/widgets/comment_text_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile/src/comments/comments.dart';
+
+
+class CommentTextField extends StatelessWidget {
+  final TextEditingController textController;
+
+  const CommentTextField({super.key, required this.textController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: TextField(
+        controller: textController,
+        onChanged: (text) {
+          context.read<CommentsCreateBloc>().add(CommentTextChanged(text));
+        },
+        maxLines: null,
+        autofocus: true,
+        decoration: const InputDecoration(
+          hintText: 'Post your reply...',
+          border: InputBorder.none,
+          counterText: '',
+          hintStyle: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+        style: Theme.of(context).textTheme.bodyLarge,
+      ),
+    );
+  }
+}
