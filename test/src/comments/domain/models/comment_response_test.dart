acee71b1a2936ab52baa32c3e1ad4c58e2696457
brillygamboa <BRILLY.GAMBOA@ucr.ac.cr>
2025-06-15 14:19:53 -0600

    test(comments): update CommentsResponse tests for clarity and completeness

--- test/src/comments/domain/models/comment_response_test.dart
+++ test/src/comments/domain/models/comment_response_test.dart
@@ -7,6 +7,8 @@ void main() {
     content: 'Comentario 1',
     username: 'user1',
     createdAt: DateTime.parse('2023-01-01T12:00:00.000Z'),
+    profileImageUrl: 'https://example.com/user1.jpg',
+    attachmentUrl: null,
   );
 
   final tCommentsResponse = CommentsResponse(
@@ -26,41 +28,40 @@ void main() {
       expect(
         tCommentsResponse,
         CommentsResponse(
-          comments: [tCommentModel1],
+          comments: [
+            CommentModel(
+              id: '1',
+              content: 'Comentario 1',
+              username: 'user1',
+              createdAt: DateTime.parse('2023-01-01T12:00:00.000Z'),
+              profileImageUrl: 'https://example.com/user1.jpg',
+              attachmentUrl: null,
+            ),
+          ],
           totalItems: 5,
           currentIndex: 0,
         ),
       );
     });
 
-     group('fromJson', () {
-      test(
-          'debe devolver un CommentsResponse válido con una lista de comentarios vacía',
-          () {
-        final Map<String, dynamic> jsonMapWithEmptyList = {
-          'comments': [],
-          'metadata': {'totalItems': 0, 'currentIndex': 0}
+    group('fromJson', () {
+      test('debe devolver un CommentsResponse válido desde un JSON completo', () {
+        final Map<String, dynamic> jsonMap = {
+          'comments': [
+            {
+              'id': '1',
+              'content': 'Comentario 1',
+              'username': 'user1',
+              'created_at': '2023-01-01T12:00:00.000Z',
+              'profile_image_url': 'https://example.com/user1.jpg',
+              'attachment_url': null,
+            }
+          ],
+          'metadata': {'totalItems': 5, 'currentIndex': 0}
         };
 
-        final result = CommentsResponse.fromJson(jsonMapWithEmptyList);
-
-        expect(result.comments, isEmpty);
-        expect(result.totalItems, 0);
-        expect(result.currentIndex, 0);
-      });
-
-      test(
-          'debe lanzar una excepción si falta una clave DENTRO de "metadata"',
-          () {
-        final Map<String, dynamic> invalidJson = {
-          'comments': [],
-          'metadata': {
-            'currentIndex': 0,
-          }
-        };
-
-        expect(() => CommentsResponse.fromJson(invalidJson),
-            throwsA(isA<TypeError>()));
+        final result = CommentsResponse.fromJson(jsonMap);
+        expect(result, tCommentsResponse);
       });
     });
   });
